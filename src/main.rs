5ad4a8896074a9c1bd65974d3ab8268e271bec28
Blanchard <feblanchard@poli.ufrj.br>
2023-05-16 00:26:52 -0300

    PC Casos de usos implementados

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,54 @@ mod utils;
 use lock_sum::study_case::{study_case, basic_test};
 use prod_cons::produter_consumer::share_memory;
 
+use std::time::{Instant, Duration};
 
 fn main() {
-    share_memory(1, 1, 1, 100);
-    study_case();
+    let n_vetor = vec![1, 10, 100, 1000];
+    let m = 100000;
+    let combination_vetor = vec![1,2,4,8];
+
+    let media = false; //Controle do desenvolvedor para compilar do jeito que desejar
+
+    for n in &n_vetor{
+
+        for nc in &combination_vetor{
+            if(media){ //Se eu quiser computar média de tempo
+                let mut tempo_total: f64 = 0.0; 
+                for i in 0..10{
+                    let start_time = Instant::now();
+                    share_memory(1, *nc, *n, m, false);
+                    let elapsed_time = start_time.elapsed().as_secs_f64();
+                    tempo_total += elapsed_time;
+                    println!("Combinação: N = {}, (Np, Nc) = ({},{}) - Tempo decorrido: {}s", *n, 1, *nc, elapsed_time);
+                }
+                println!("Média: {}s \n", tempo_total/10.0);
+            }else{ //Se eu quiser só rodar o código
+                share_memory(1, *nc, *n, m, false);
+            }
+        }
+        for np in &combination_vetor{
+            if *np == 1 {
+                continue;
+            }
+            if(media){ //Se eu quiser computar média de tempo
+                let mut tempo_total: f64 = 0.0; 
+                for i in 0..10{
+                    let start_time = Instant::now();
+                    share_memory(*np, 1, *n, m, false);
+                    let elapsed_time = start_time.elapsed().as_secs_f64();
+                    tempo_total += elapsed_time;
+                    println!("Combinação: N = {}, (Np, Nc) = ({},{}) - Tempo decorrido: {}s", *n, *np, 1, elapsed_time);
+                }
+                println!("Média: {}s \n", tempo_total/10.0);
+            }else{ //Se eu quiser só rodar o código
+                share_memory(*np, 1, *n, m, false);
+            }
+
+
+        }
+    }
+
+    //study_case();
     //basic_test();
 }
--- src/prod_cons/prod_cons.rs
+++ src/prod_cons/prod_cons.rs
@@ -1,15 +1,19 @@
 use std::sync::{Arc, Mutex, Condvar};
 use std::thread;
 use rand::Rng;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 const MAX: i32 = 1_000_000_000; // valor máximo dos números gerados
 
-pub fn share_memory(np: i32, nc: i32, n: usize, m: i32) {
+pub fn share_memory(np: i32, nc: i32, n: usize, m: i32, showPrint: bool) {
     let buffer = Arc::new(Mutex::new(vec![0; n])); // vetor compartilhado dentro de um Mutex
     let (empty, full) = (Arc::new(Condvar::new()), Arc::new(Condvar::new())); // variáveis de condição para sincronização
     let mut handles = vec![]; // vetor para armazenar as threads
     let produced = Arc::new(Mutex::new(0));
     let consumed = Arc::new(Mutex::new(0));
+
+    let mut filled_vetor = Arc::new(Mutex::new(vec![]));
 
     // cria threads Produtores
     for i in 0..np {
@@ -17,6 +21,7 @@ pub fn share_memory(np: i32, nc: i32, n: usize, m: i32) {
         let empty = Arc::clone(&empty);
         let full = Arc::clone(&full);
         let produced = Arc::clone(&produced);
+
         let handle = thread::spawn(move || {
             let mut rng = rand::thread_rng();
             loop {
@@ -35,7 +40,10 @@ pub fn share_memory(np: i32, nc: i32, n: usize, m: i32) {
                 }
                 let pos = buffer.iter().position(|&x| x == 0).unwrap();
                 buffer[pos] = num;
-                println!("Produção n° {} - Produtor {} colocou na posição {} o número {}", temp_qty_produced+1, i, pos, num);
+
+                if(showPrint){
+                    println!("Produção n° {} - Produtor {} colocou na posição {} o número {}", temp_qty_produced+1, i, pos, num);
+                }
                 full.notify_one(); // notifica liberação para consumidor
             }
         });
@@ -48,6 +56,7 @@ pub fn share_memory(np: i32, nc: i32, n: usize, m: i32) {
         let empty = Arc::clone(&empty);
         let full = Arc::clone(&full);
         let consumed = Arc::clone(&consumed);
+        let filled_vetor = Arc::clone(&filled_vetor);
         let handle = thread::spawn(move || {
             loop {
                 let mut qty_consumed = consumed.lock().unwrap();
@@ -64,7 +73,14 @@ pub fn share_memory(np: i32, nc: i32, n: usize, m: i32) {
                 }
                 let pos = buffer.iter().position(|&x| x != 0).unwrap();
                 let num = buffer[pos];
+
+                // Construção dos dados para o gráfico de preenchimento do buffer
+                let fill = buffer.iter().filter(|&num| *num != 0).count();
+                filled_vetor.lock().unwrap().push(fill);
+                //
+
                 buffer[pos] = 0;
+                
                 empty.notify_one();
 
                 let mut result = "";
@@ -73,9 +89,9 @@ pub fn share_memory(np: i32, nc: i32, n: usize, m: i32) {
                 } else {
                     result = "não é primo!";
                 }
-
-                println!("Consumo n° {} - Consumidor {} retirou da posição {} o número {}, resultado: {}", temp_qty_consumed+1, i, pos, num, result);
-            
+                if(showPrint){
+                    println!("Consumo n° {} - Consumidor {} retirou da posição {} o número {}, resultado: {}", temp_qty_consumed+1, i, pos, num, result);
+                }
             }
         });
         handles.push(handle);
@@ -85,6 +101,15 @@ pub fn share_memory(np: i32, nc: i32, n: usize, m: i32) {
     for handle in handles {
         handle.join().unwrap();
     }
+
+    let filename = format!("./results/PC_FilledVector_{}_{}.{}.txt",n,np,nc);
+    let mut f = OpenOptions::new()
+                .create(true)
+                .append(true)
+                .open(filename)
+                .expect("Ocorreu um erro na abertura do arquivo!");
+    writeln!(f, "{:?}", filled_vetor.lock().unwrap()).expect("Erro na escrita do arquivo.");
+    println!("Arquivo {}_{}.{} escrito", n,np,nc);
 }
 
 fn is_prime(num: i32) -> bool {
